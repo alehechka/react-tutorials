16a92c68ed81dcca4fee90ab50fa2eddd5e195d1
alehechka <42357034+alehechka@users.noreply.github.com>
2020-04-26 23:18:00 -0500

    YT-react-redux-firebase

---
+++ yt-react-redux-app/src/config/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+//Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDdN0BxLf4iYNwYzwvqOgHch2QNNHyrvjs",
+  authDomain: "yt-react-redux-app.firebaseapp.com",
+  databaseURL: "https://yt-react-redux-app.firebaseio.com",
+  projectId: "yt-react-redux-app",
+  storageBucket: "yt-react-redux-app.appspot.com",
+  messagingSenderId: "881279966574",
+  appId: "1:881279966574:web:8a35962da4c411e69c357f",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase; 
